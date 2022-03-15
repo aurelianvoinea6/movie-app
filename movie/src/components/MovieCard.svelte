c3dd08b2ccc8d89914c20f1ce32c7c8e1a681d6d
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-03-15 15:28:36 +0100

    styled the cards

--- movie/src/components/MovieCard.svelte
+++ movie/src/components/MovieCard.svelte
@@ -4,4 +4,16 @@
 
 <div class="movie-card">
 	<img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt={movie.title} />
+	<div class="description">
+		<h2>{movie.title}</h2>
+		<p>{movie.release_date}</p>
+	</div>
 </div>
+
+<style>
+	img {
+		width: 100%;
+		height: 30vh;
+		object-fit: cover;
+	}
+</style>
