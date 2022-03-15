5383a40d31986c2b838e0a0fb6b28793ae377968
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-03-15 15:32:47 +0100

    styled the cards and made them responsive

--- movie/src/components/MovieCard.svelte
+++ movie/src/components/MovieCard.svelte
@@ -15,5 +15,22 @@
 		width: 100%;
 		height: 30vh;
 		object-fit: cover;
+		border-radius: 1rem;
+		margin-bottom: 1rem;
+	}
+	h2 {
+		font-size: 0.9rem;
+	}
+	.description {
+		height: 5vh;
+	}
+	p {
+		font-size: 0.7rem;
+	}
+	.movie-card {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		padding: 1rem;
 	}
 </style>
