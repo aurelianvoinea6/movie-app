e72ea598d52bd45cea0697547a588e1717b1b395
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-03-15 15:42:15 +0100

    getting the movies by id

---
+++ movie/src/routes/movie/[id].svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	export async function load({ fetch, params }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/${params.id}?api_key=f3146106483004e6072b34c9e290fa28&language=en-US`
+		);
+		const movieDetail = await res.json();
+
+		if (res.ok) {
+			return {
+				props: { movieDetail }
+			};
+		}
+	}
+</script>
