8ef19b15052c3b554489357fb13d1b229340d8ac
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-03-15 15:14:14 +0100

    Initial commit

---
+++ movie/src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?api_key=f3146106483004e6072b34c9e290fa28&language=en-US&page=1`
+		);
+		const data = await res.json();
+
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
+	}
+</script>
+
+<script>
+	import PopularMovies from '../components/PopularMovies.svelte';
+	export let popular;
+</script>
+
+<section>
+	<PopularMovies {popular} />
+</section>
