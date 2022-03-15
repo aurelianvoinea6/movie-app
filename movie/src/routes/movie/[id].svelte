56908bc395a252df7faf7afd7d7aa1346bff7fb9
Aurelian <70028912+aurelianvoinea6@users.noreply.github.com>
2022-03-15 15:50:30 +0100

    fetched info about the movies from the API

--- movie/src/routes/movie/[id].svelte
+++ movie/src/routes/movie/[id].svelte
@@ -12,3 +12,29 @@
 		}
 	}
 </script>
+
+<script>
+	export let movieDetail;
+</script>
+
+<div class="movie-details">
+	<div class="img-container">
+		<img
+			src={'https://image.tmdb.org/t/p/original' + movieDetail.backdrop_path}
+			alt={movieDetail.title}
+		/>
+	</div>
+	<div class="txt-container">
+		<h1>{movieDetail.title}</h1>
+		<p class="overviw">{movieDetail.overview}</p>
+		<p>
+			<span>Release Date</span>
+			{movieDetail.release_date} <br />
+			<span>Budget:</span> ${movieDetail.budget} <br />
+			<span>Rating:</span>
+			{movieDetail.vote_average} <br />
+			<span>Runtime:</span>
+			{movieDetail.runtime}mins
+		</p>
+	</div>
+</div>
